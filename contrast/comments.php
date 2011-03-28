b86e8ceeeb4241b83978d8a0d718975b8699245d
Nuno Morgadinho <nuno.morgadinho@gmail.com>
2011-03-28 12:50:09 +0100

    adding new template blog

--- contrast/comments.php
+++ contrast/comments.php
@@ -5,9 +5,11 @@
 		<p class="nocomments">This post is password protected. Enter the password to view comments.</p>
 	<?php return; } ?>
 	
-<?php if ( have_comments() ) : ?>
-	
-	<h3 id="comments"><?php comments_number('No Responses', '1 COMMENT', '% COMMENTS' ); ?></h3>
+<?php if ( have_comments() ) : ?> 
+		
+	<?php global $wp_query; $carray =$wp_query->comments; $n_comments = count($carray); ?>	
+		
+	<h3 id="comments"><?php echo $n_comments.' '; _e('Comments'); ?></h3>
 	
 	<div class="navigation">
 		<div class="alignleft"><?php previous_comments_link() ?></div>
@@ -47,19 +49,19 @@
 	
 	<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
 		<?php if ( is_user_logged_in() ) : ?>
-		<p><label for="username"><small>Logged in as</small></label>
+		<p><label for="username"><small><?php _e('Logged in'); ?></small></label>
 		<a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php" id="username"><?php echo $user_identity; ?></a> <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="Log out of this account">Log out &raquo;</a></p>
 		
 		<?php else : ?>
-		<p><label for="author"><small>Name <?php //if ($req) echo "(required)"; ?></small></label>
+		<p><label for="author"><small><?php _e('Name');  //if ($req) echo "(required)"; ?></small></label>
 		<input type="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> /></p>
-		<p><label for="email"><small>Mail <?php //if ($req) echo "(required)"; ?></small></label>
+		<p><label for="email"><small><?php _e('Mail'); //if ($req) echo "(required)"; ?></small></label>
 		<input type="text" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> /></p>
 		<p><label for="url"><small>Website</small></label>
 		<input type="text" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" size="22" tabindex="3" /></p>
 		<?php endif; ?>
 		
-		<p class="comment-textarea"><label for="comment"><small>Comment</small></label>
+		<p class="comment-textarea"><label for="comment"><small><?php _e('Comment'); ?></small></label>
 		<textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea>
 		<input name="submit" type="submit" id="submit" tabindex="5" value="" /></p>
 		<?php comment_id_fields(); ?>
