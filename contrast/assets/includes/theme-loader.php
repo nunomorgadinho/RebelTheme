1be8c99a16fa5fcc320e1b4ba81abf22e987d406
Nuno Morgadinho <nuno.morgadinho@gmail.com>
2011-03-31 23:40:49 +0100

    test

--- contrast/assets/includes/theme-loader.php
+++ contrast/assets/includes/theme-loader.php
@@ -1,7 +1,8 @@
 <?php if ($_GET['type'] == 'html5video') { ?>
 <video id="video_element" height="auto" width="100%" autoplay="autoplay" loop="loop">
-	<source src="<?php echo $_GET['background_image']; ?>" type="video/mp4" /><!-- Safari / iOS video    -->
+	<source src="<?php echo $_GET['background_image']; ?>" type="video/mp4" /><!-- Safari / iOS video / IE9   -->
 	<source src="<?php echo $_GET['ogg']; ?>" type="video/ogg" /><!-- Firefox / Opera / Chrome10 -->
+	<embed src="http://blip.tv/play/gcMVgcmBAgA%2Em4v" type="application/x-shockwave-flash" width="1024" height="798" allowscriptaccess="always" allowfullscreen="true"></embed>
 </video>
 <?php } else { ?>
 <img src="<?php echo $_GET['background_image']; ?>" alt="" />
