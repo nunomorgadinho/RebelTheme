230d70507a7fae0ca36bc9d5b5271a125a0be41e
Nuno Morgadinho <nuno.morgadinho@gmail.com>
2011-03-31 23:50:19 +0100

    test

--- contrast/assets/includes/theme-loader.php
+++ contrast/assets/includes/theme-loader.php
@@ -1,5 +1,5 @@
 <?php if ($_GET['type'] == 'html5video') { ?>
-<video id="video_element" height="auto" width="100%" autoplay="autoplay" loop="loop">
+<video id="video_element2" height="auto" width="100%" autoplay="autoplay" loop="loop">
 	<source src="<?php echo $_GET['background_image']; ?>" type="video/mp4" /><!-- Safari / iOS video / IE9   -->
 	<source src="<?php echo $_GET['ogg']; ?>" type="video/ogg" /><!-- Firefox / Opera / Chrome10 -->
 
