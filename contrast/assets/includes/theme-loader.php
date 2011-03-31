beb9dfa4be96ad271553fceeaa8935672b74a4a5
Nuno Morgadinho <nuno.morgadinho@gmail.com>
2011-03-31 23:43:59 +0100

    test

--- contrast/assets/includes/theme-loader.php
+++ contrast/assets/includes/theme-loader.php
@@ -2,7 +2,12 @@
 <video id="video_element" height="auto" width="100%" autoplay="autoplay" loop="loop">
 	<source src="<?php echo $_GET['background_image']; ?>" type="video/mp4" /><!-- Safari / iOS video / IE9   -->
 	<source src="<?php echo $_GET['ogg']; ?>" type="video/ogg" /><!-- Firefox / Opera / Chrome10 -->
-	<embed src="http://blip.tv/play/gcMVgcmBAgA%2Em4v" type="application/x-shockwave-flash" width="1024" height="798" allowscriptaccess="always" allowfullscreen="true"></embed>
+
+		<script type="text/javascript">
+			var flashvars = { flv : '<?php echo $_GET['ogg']; ?>' };
+			var params = { wmode: 'transparent' };
+			swfobject.embedSWF('<?php bloginfo('template_url'); ?>/assets/flash/background.swf', 'background', '100%', '100%', '9.0.0', 'expressInstall.swf', flashvars, params);
+		</script>
 </video>
 <?php } else { ?>
 <img src="<?php echo $_GET['background_image']; ?>" alt="" />
