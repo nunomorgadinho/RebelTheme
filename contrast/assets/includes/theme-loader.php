3329222df06c12b2f1d614348d332e43388ffb1f
Nuno Morgadinho <nuno.morgadinho@gmail.com>
2011-04-05 17:27:11 +0100

    small fix

--- contrast/assets/includes/theme-loader.php
+++ contrast/assets/includes/theme-loader.php
@@ -1,5 +1,5 @@
 <?php if ($_GET['type'] == 'html5video') { ?>
-<video id="video_element2" height="auto" width="100%" autoplay="autoplay" loop="loop">
+<video id="video_element2" height="100%" width="100%" autoplay="autoplay" loop="loop">
 	<source src="<?php echo $_GET['background_image']; ?>" type="video/mp4" /><!-- Chrome / Safari / iOS video / IE9   -->
 	<source src="<?php echo $_GET['ogg']; ?>" type="video/ogg" /><!-- Firefox -->
   
