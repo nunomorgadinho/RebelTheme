e4aaa07832bc0cdc683a1c822971e6be39da80f2
Nuno Morgadinho <nuno.morgadinho@gmail.com>
2011-04-07 18:21:36 +0100

    tesT

--- contrast/assets/includes/theme-loader.php
+++ contrast/assets/includes/theme-loader.php
@@ -4,7 +4,14 @@
 	<source src="<?php echo $_GET['background_image']; ?>" type="video/mp4" /><!-- Chrome / Safari / iOS video / IE9   -->
 	<source src="<?php echo $_GET['ogg']; ?>" type="video/ogg" /><!-- Firefox -->
 </video>
-
+<script type="text/javascript">
+	var v = document.createElement("video"); // Are we dealing with a browser that supports <video>? 
+    if ( !v ) { // If no, use Flash
+		var flashvars = { flv : '<?php echo $_GET['fallback']; ?>' };
+		var params = { wmode: 'transparent' };
+		swfobject.embedSWF('http://rebelact.me/wp-content/themes/RebelTheme/contrast/assets/flash/background.swf', 'background', '100%', '100%', '10.0.0', 'expressInstall.swf', flashvars, params);
+    } 
+</script>
 
 <?php } else { ?>
 <img src="<?php echo $_GET['background_image']; ?>" alt="no video found" />
