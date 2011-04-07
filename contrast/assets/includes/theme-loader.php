f8b3940dc34a5228dd92158b250d1a2397f9e04f
Nuno Morgadinho <nuno.morgadinho@gmail.com>
2011-04-07 15:18:35 +0100

    I8 video to show

--- contrast/assets/includes/theme-loader.php
+++ contrast/assets/includes/theme-loader.php
@@ -2,8 +2,8 @@
 <video id="video_element2" height="auto" width="100%"  style='height:auto; width:100%;' autoplay="autoplay" loop="loop">
 	<source src="<?php echo $_GET['background_image']; ?>" type="video/mp4" /><!-- Chrome / Safari / iOS video / IE9   -->
 	<source src="<?php echo $_GET['ogg']; ?>" type="video/ogg" /><!-- Firefox -->
-  
-	<script type="text/javascript">
+</video>
+<script type="text/javascript">
 	var v = document.createElement("video"); // Are we dealing with a browser that supports <video>? 
     if ( !v ) { // If no, use Flash
 		var flashvars = { flv : '<?php echo $_GET['fallback']; ?>' };
@@ -11,7 +11,6 @@
 		swfobject.embedSWF('http://rebelact.me/wp-content/themes/RebelTheme/contrast/assets/flash/background.swf', 'background', '100%', '100%', '10.0.0', 'expressInstall.swf', flashvars, params);
     }
 	</script>
-</video>
 <?php } else { ?>
 <img src="<?php echo $_GET['background_image']; ?>" alt="no video found" />
 <?php } ?>
