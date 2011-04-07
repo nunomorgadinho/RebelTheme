41aa4c9fbb20429fdde1574b836d66faf7fb34b7
Nuno Morgadinho <nuno.morgadinho@gmail.com>
2011-04-07 18:23:08 +0100

    tesT

--- contrast/assets/includes/theme-background.php
+++ contrast/assets/includes/theme-background.php
@@ -83,9 +83,11 @@
 		<?php } elseif (meta(array('id' => $post->ID, 'meta' => 'post_background')) == '5') { ?>
 			
 			<script type="text/javascript">
+			if (BrowserDetect.browser == "Explorer") {
 				var flashvars = { flv : '<?php echo meta(array('id' => $post->ID, 'meta' => 'post_background_flash_video')); ?>' };
 				var params = { wmode: 'transparent' };
 				swfobject.embedSWF('<?php bloginfo('template_url'); ?>/assets/flash/background.swf', 'background', '100%', '100%', '9.0.0', 'expressInstall.swf', flashvars, params);
+			}
 			</script>
 		
 		<?php } elseif (meta(array('id' => $post->ID, 'meta' => 'post_background')) == '4') { ?>
